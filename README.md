02fb7a5f431a69c8272d25bbacc8c547e1c9285a
Arky <hitmanarky@gmail.com>
2015-12-11 07:09:35 -0500

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# FxOS-keyboards
- Online demo page for Mozilla Firefox OS Keyboard  The goal is update Jan's and Tim's work. http://timdream.org/gaia-keyboard-demo/ https://github.com/janjongboom/gaia-keyboard-demo
+# Gaia Keyboard Demo
+
+This is a off-work fun project to bring the built-in keyboard of [Mozilla Firefox OS](https://mozilla.org/firefoxos/) to the web, and make available on any device. 
+
+## Underneath
+
+It pull the keyboard codebase from [Gaia](https://github.com/mozilla-b2g/gaia), and re-implements `navigator.mozInputMethod` API so we could communicate between the fake input and the app.
+
+The input has to be a fake one so the native keyboard will not pop-up when you try this page on a touchscreen.
+
+Additionally, other missing APIs that is not provided on Safari Mobile are shimmed.
---
+++ assets/demo/settings.js
@@ -0,0 +1,99 @@
+'use strict';
+
+(function(exports) {
+
+var SettingsHandler = function(app) {
+  this.app = app;
+
+  this.settings = new Map();
+  this.settings.set('keyboard.wordsuggestion', true);
+  this.settings.set('keyboard.autocorrect', true);
+  this.settings.set('keyboard.vibration', true);
+  this.settings.set('audio.volume.notification', 10);
+};
+
+SettingsHandler.prototype.start = function() {
+  // Turn off sound feedback if this platform does not support audio.
+  var canPlayOgg = (new Audio()).canPlayType('audio/ogg');
+  this.settings.set('keyboard.clicksound', !!canPlayOgg);
+
+  window.addEventListener('click', this);
+
+  if (!canPlayOgg) {
+    var el = document.querySelector('[data-setting-id="keyboard.clicksound"]');
+    if (el) {
+      el.disabled = true;
+    }
+  }
+
+  this.settings.forEach(function(value, key) {
+    var el = document.querySelector('[data-setting-id="' + key + '"]');
+    if (!el) {
+      return;
+    }
+
+    el.checked = value;
+  }, this);
+};
+
+SettingsHandler.prototype.handleEvent = function(evt) {
+  if (!('settingId' in evt.target.dataset)) {
+    return;
+  }
+
+  var key = evt.target.dataset.settingId;
+  var value = evt.target.checked;
+  this.settings.set(key, value);
+
+  this.app.postMessage({
+    api: 'settings',
+    method: 'dispatchSettingChange',
+    key: key,
+    value: value
+  })
+};
+
+SettingsHandler.prototype.handleMessage = function(data) {
+  switch (data.method) {
+    case 'get':
+      var result = {};
+      result[data.args[0]] = this.settings.get(data.args[0]);
+      this.app.postMessage({
+        api: data.api,
+        lockId: data.lockId,
+        id: data.id,
+        result: result
+      });
+
+      break;
+
+    case 'set':
+      var key;
+      for (key in data.args[0]) {
+        this.settings.set(key, data.args[0][key]);
+      }
+      this.app.postMessage({
+        api: data.api,
+        lockId: data.lockId,
+        id: data.id,
+        result: data.args[0]
+      });
+
+      break;
+
+    case 'clear':
+      this.settings.delete(data.args[0]);
+      this.app.postMessage({
+        api: data.api,
+        lockId: data.lockId,
+        id: data.id,
+        result: data.args[0]
+      });
+
+      break;
+  }
+};
+
+exports.SettingsHandler = SettingsHandler;
+
+}(window));
